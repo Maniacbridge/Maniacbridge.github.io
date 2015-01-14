1ad1b53896397a9c9d433aa9e84481165c8d6b4c
Maniacbridge <nathan@Nathan.local>
2015-01-14 08:22:38 -0600

    v1.0.3.1
    slight improvement to the jQeury function

--- effects.js
+++ effects.js
@@ -1,9 +1,12 @@
 $(document).ready(function ()
 {
+    $("div p").hide();
     $("div").click(function ()
     {
         $("div").fadeOut('slow');
-    });     
+        $("div p").show();
+        $("div p").fadeOut("slow");
+    });    
 });
 
 
