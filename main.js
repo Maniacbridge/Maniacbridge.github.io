2013096a7bf283d75fb57ca4e196efa1fa22a821
Maniacbridge <maniacbridge115@gmail.com>
2017-11-14 13:42:19 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,14 +5,19 @@ var spd;
 var def;
 var mdef;
 function hitrate(){
-  var weapon = prompt("What is the hit rate?")
-  var roll1 = Math.floor(Math.random() * 100)
-  var roll2 = Math.floor(Math.random() * 100)
+  var weapon = prompt("What is the hit rate?");
+  var roll1 = Math.floor(Math.random() * 100);
+  var roll2 = Math.floor(Math.random() * 100);
   chance = (roll1 + roll2)/2;
   if (weapon > chance){
-    alert("Your weapon's hit rate was " + weapon + ". The hit rate needed to succeed was " + chance +". You hit!")
+    alert("Your weapon's hit rate was " + weapon + ". The hit rate needed to succeed was " + chance +". You hit!");
   }
   else{
-    alert("Your weapon's hit rate was " + weapon + ". The hit rate needed to succeed was " + chance + ". You missed.")
+    alert("Your weapon's hit rate was " + weapon + ". The hit rate needed to succeed was " + chance + ". You missed.");
   }
+  var yeet = prompt("This is a super test bois");
+  topEdit(yeet);
+}
+function topEdit(text){
+  div.getElementById(topbox).innerHTML = text;
 }
