3817fc801f1712dec25ec83098e8e102601818df
Maniacbridge <maniacbridge115@gmail.com>
2017-11-14 10:41:46 -0700

    Add files via upload
    Test #1: font

---
+++ main.js
@@ -0,0 +1,18 @@
+//ohmaniamnotgoodwithcomputerplztohelp
+var hp;
+var atk;
+var spd;
+var def;
+var mdef;
+function hitrate(){
+  var weapon = prompt("What is the hit rate?")
+  var roll1 = Math.floor(Math.random() * 100)
+  var roll2 = Math.floor(Math.random() * 100)
+  chance = (roll1 + roll2)/2;
+  if (weapon > chance){
+    alert("Your weapon's hit rate was " + weapon + ". The hit rate needed to succeed was " + chance +". You hit!")
+  }
+  else{
+    alert("Your weapon's hit rate was " + weapon + ". The hit rate needed to succeed was " + chance + ". You missed.")
+  }
+}
