a2a2991739961e534cd65c159502c0e2e9092a25
Maniacbridge <maniacbridge115@gmail.com>
2017-11-15 20:34:46 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 //ohmaniamnotgoodwithcomputerplztohelp
-var hp;
-var atk;
-var spd;
-var def;
-var mdef;
-function hitrate(){
+$(document).ready(function(){
+  //testing testing 1 2 1 2
+  console.log("hello world!")
+  //defining variables
+  var hp;
+  var atk;
+  var spd;
+  var def;
+  var mdef;
+  var determine = 0;
+  naming();
+  function naming(){
+    var name = prompt("What is your name?");
+    determine = 1;
+    topEdit("So your name is " + name + "?");
+  }
+  function topEdit(text) {
+    document.getElementById("topbox").innerHTML = text;
+  }
+  
+
+});
+/* function hitrate(){
   var weapon = prompt("What is the hit rate?");
   var roll1 = Math.floor(Math.random() * 100);
   var roll2 = Math.floor(Math.random() * 100);
@@ -15,9 +32,4 @@ function hitrate(){
   else{
     alert("Your weapon's hit rate was " + weapon + ". The hit rate needed to succeed was " + chance + ". You missed.");
   }
-  var yeet = prompt("This is a super test bois");
-  topEdit(yeet);
-}
-function topEdit(text){
-  document.getElementById("topbox").innerHTML = text;
-}
+} */
