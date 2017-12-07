24442d09b485a7318018cd592aa78dffcbc07995
Maniacbridge <maniacbridge115@gmail.com>
2017-12-07 09:29:26 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,45 +1,15 @@
+Game = {};
+Game.start = function(){
+  Game.hp;
+  Game.atk;
+  Game.mag;
+  Game.spd;
+  Game.avo;
+  Game.def;
+  Game.res;
+  Game.topbox = function(){
+    var lol = prompt("this is a test");
+    document.getElementById("topbox").innerHTML = lol;
+  }
+}
 //ohmaniamnotgoodwithcomputerplztohelp
-$(document).ready(function(){
-  //testing testing 1 2 1 2
-  console.log("hello world!");
-  
-  //defining variables
-  var hp;
-  var atk;
-  var spd;
-  var def;
-  var mdef;
-  var choice = false;
-  
-  //open game on asking your name
-  naming();
-  function naming(){
-    topEdit("What is your name?");
-    var name = document.getElementById("name").innerHTML;
-  }
-  
-  //Writes text and determines position in game
-  function topEdit(text) {
-    document.getElementById("topbox").innerHTML = text;
-    yesNo();
-  }
-  
-  
-  //testing yes and no here
-  function yesNo(){
-    
-  }
-
-});
-/* function hitrate(){
-  var weapon = prompt("What is the hit rate?");
-  var roll1 = Math.floor(Math.random() * 100);
-  var roll2 = Math.floor(Math.random() * 100);
-  chance = (roll1 + roll2)/2;
-  if (weapon > chance){
-    alert("Your weapon's hit rate was " + weapon + ". The hit rate needed to succeed was " + chance +". You hit!");
-  }
-  else{
-    alert("Your weapon's hit rate was " + weapon + ". The hit rate needed to succeed was " + chance + ". You missed.");
-  }
-} */
